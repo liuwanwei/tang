ebdd751a85117d37ab0bb1fcb71c3fbae5363c32
sungeoliu <liuwanwei@sungeo.cn>
2013-09-22 20:44:33 +0800

    提交错过提交的文件

---
+++ protected/views/create.php
@@ -0,0 +1,43 @@
+<?php
+/* @var $this VoteController */
+/* @var $model Vote */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'vote-create-form',
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'user_id'); ?>
+		<?php echo $form->textField($model,'user_id'); ?>
+		<?php echo $form->error($model,'user_id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'restaurant_id'); ?>
+		<?php echo $form->textField($model,'restaurant_id'); ?>
+		<?php echo $form->error($model,'restaurant_id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'rating'); ?>
+		<?php echo $form->textField($model,'rating'); ?>
+		<?php echo $form->error($model,'rating'); ?>
+	</div>
+
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Submit'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
