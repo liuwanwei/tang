ad3f4f103d1f82f67c73d23ece6c49f4b16c8595
sungeoliu <liuwanwei@sungeo.cn>
2013-10-21 10:44:07 +0800

    页面底部增加统计代码

--- protected/views/layouts/main-tang.php
+++ protected/views/layouts/main-tang.php
@@ -174,5 +174,8 @@ $(".areamenu").hover(function(){
 });
 </script>
 
+<!-- 统计 -->
+<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1000119341'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s22.cnzz.com/z_stat.php%3Fid%3D1000119341%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
+
 </body>
 </html>
