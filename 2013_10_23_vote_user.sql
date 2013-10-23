19ee276227eba12dcfc009debe52c907b85d518b
mao yu <maoyu417@163.com>
2013-10-23 13:50:03 +0800

    修改user表extension_user_id字段类型；vote表添加create_datetime字段

---
+++ 2013_10_23_vote_user.sql
@@ -0,0 +1,2 @@
+ALTER TABLE vote ADD COLUMN `create_datetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE  `user` CHANGE  `extension_user_id`  `extension_user_id` BIGINT( 11 ) NOT NULL;
