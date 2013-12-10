3f8a352ad4bc51a4c23f2f16287e2dd742171186
maoyu <maoyu417@gmail.com>
2013-12-10 09:56:34 +0800

    修改新浪微博的key值

--- protected/extensions/oauthLogin/config/main.php
+++ protected/extensions/oauthLogin/config/main.php
@@ -2,7 +2,7 @@
 /**
  * @var 新浪配置
  */
-define( "WB_AKEY" , '2989285180' );
-define( "WB_SKEY" , '8e43950fd0c3fd5fefe5c7ebbd7dcc93' );
+define( "WB_AKEY" , '3495571392');
+define( "WB_SKEY" , 'de8a8905a2048905f3e18a8a9365ddfe');
 define( "WB_CALLBACK_URL" , 'http://www.laotangguan.com/site/wbLogin');
 
