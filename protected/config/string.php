10ed4cf6239d2e4419658037ae74a26938f8ba53
maoyu <maoyu417@gmail.com>
2013-12-12 15:40:33 +0800

    新建string.php文件夹

---
+++ protected/config/string.php
@@ -0,0 +1,8 @@
+<?php
+
+define('ERROR_CODE_LOGIN_REQUIRE', -1);
+define('ERROR_CODE_LOGIN_REQUIRE_MESSAGE', '您还没有登陆，请登陆');
+define('ERROR_CODE_FREQUENCY', -2);
+define('ERROR_CODE_FREQUENCY_MESSAGE', '您操作频率过快，请稍后再操作');
+
+define('DELAY', 3);
