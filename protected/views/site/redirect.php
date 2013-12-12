38238b0d9e98563e41f8c3e085c6ba7805817dfc
liangxiaobo <liangxiaobo2009@yeah.net>
2013-12-12 22:17:02 +0800

    给过度页加了一个简单的样式

--- protected/views/site/redirect.php
+++ protected/views/site/redirect.php
@@ -1,18 +1,24 @@
 <?php 
 $this->layout="main-tang";
+Yii::app()->clientScript->registerScriptFile('//code.jquery.com/jquery-1.10.2.min.js');
 ?>
+<div class="g-alert">
+	<div>
+		<i class="fa fa-bullhorn"></i><span class="alertmsg"><?php echo $message; ?></span>
+	</div>
 <div>
-<?php echo $message; ?>
+	<span id="time">0</span>秒后，跳转
 </div>
-<span id="time"></span>
+</div>
 <script type="text/javascript">
-	var i=0;
-	var timejishi= setInterval(function(){
-		i++;
-		document.getElementById('time').innerText=i+"";
-		if (i>=3) {
-			clearInterval(timejishi);
-			window.location="<?php echo $url; ?>";
-		}
-	},1000);
+var i=0;
+var timejishi= setInterval(function(){
+	i++;
+	document.getElementById('time').innerText=i+"";
+	if (i>=3) {
+		clearInterval(timejishi);
+		window.location="<?php echo $url; ?>";
+	}
+},1000);
+$('.g-alert').animate({opacity:0.9},3000);
 </script>
