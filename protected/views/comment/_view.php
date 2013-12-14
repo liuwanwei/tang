7421531c4d14f7b0b1de8b92afbf3f54e9a39a8b
liuwanwei <liuwanwei@gmail.com>
2013-12-14 08:55:01 +0800

    调整评论块的样式，把删除按钮放到最后边。

--- protected/views/comment/_view.php
+++ protected/views/comment/_view.php
@@ -14,17 +14,18 @@
 		?>
 	</div>
 	<div class="feed-main">
-		<div class="source"> 
-			<?php echo CHtml::encode($data->user->nick_name); ?>
-			<span class="time"><?php echo CHtml::encode($data->create_datetime); ?></span>
+		<div class="info">
+			<span class="source"><?php echo CHtml::encode($data->user->nick_name);?></span>
+			<span class="time"><?php echo CHtml::encode($data->create_datetime);?></span>
+			<div style="float:right;text-align=right;">
+			<?php if (User::model()->isAdmin()) {
+				$actionUrl = $this->createUrl('comment/delete', array('id'=>$data->id));
+				echo CHtml::link('<i class="fa fa-times comment-del" title="删除评论"></i>', $actionUrl);
+			}?>
+			</div>
 		</div>
-		
-		<div class="content">
-			<pre><?php echo $data->content ?></pre>
+		<div class="content">			
+			<pre><?php echo $data->content ?></pre>			
 		</div>
-		<?php if (User::model()->isAdmin()) {
-			$actionUrl = $this->createUrl('comment/delete', array('id'=>$data->id));
-			echo CHtml::link('<i class="fa fa-times comment-del" title="删除评论"></i>', $actionUrl);
-		}?>
 	</div>
 </div>
